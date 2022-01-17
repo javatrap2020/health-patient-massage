101b2f1db116722c733108d53b51d09521d2093c
Oleh Boiko <javatrap2020@gmail.com>
2022-01-17 19:09:48 +0200

    init

---
+++ src/main/java/com/javatrap/healthpatientmassage/HealthPatientMassageApplication.java
@@ -0,0 +1,13 @@
+package com.javatrap.healthpatientmassage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPatientMassageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPatientMassageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatrap/healthpatientmassage/HealthPatientMassageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatrap.healthpatientmassage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPatientMassageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
